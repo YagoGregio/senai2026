78f610e8fac7826495276095935088145f6f39b1
YagoGregio <yagogregio@gmail.com>
2026-09-23 15:58:10 -0300

    att

--- BCD/MySQL/db_senai.sql
+++ BCD/MySQL/db_senai.sql
@@ -23,3 +23,35 @@ CREATE TABLE venda(
     id_produto INT NOT NULL,
     dt_entrada DATE NOT NULL
 );
+USE db_senai;
+
+INSERT INTO cliente (nome_cliente,email,dt_nasc)
+    VALUES ("Michael Jackson","m.jackson@gmail.com","1958-08-29");
+
+INSERT INTO cliente (nome_cliente,email,dt_nasc)
+    VALUES ("Ayrton Senna","ayrton.senna@gmail.com","1960-03-21");
+
+INSERT INTO cliente (nome_cliente,email,dt_nasc)
+    VALUES ("Lewis Hamilton","lewis.hamilton@gmail.com","1985-01-07");
+
+USE db_senai;
+
+INSERT INTO produto (produto,dt_entrega,preco,qtd)
+    VALUES ("Monitor Dell","2026-08-23","2800.00",4);
+
+INSERT INTO produto (produto,dt_entrega,preco,qtd)
+    VALUES ("Mouse Dell","2026-05-06","125.00",3);
+
+INSERT INTO produto (produto,dt_entrega,preco,qtd)
+    VALUES ("Teclado Dell","2026-08-03","125.00",5);
+
+USE db_senai;
+
+INSERT INTO venda (id_cliente,id_produto,dt_entrada)
+    VALUES (1,1,"2026-09-25");
+
+INSERT INTO venda (id_cliente,id_produto,dt_entrada)
+    VALUES (3,2,"2022-11-23");
+
+INSERT INTO venda (id_cliente,id_produto,dt_entrada)
+    VALUES (2,3"2023-09-25");
